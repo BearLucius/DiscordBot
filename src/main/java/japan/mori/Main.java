fa77159d63d72377fb6e05913d1c28be29902770
Lucius <enotik455@mail.ru>
2024-02-29 15:06:56 +0300

    First Thing

---
+++ src/main/java/japan/mori/Main.java
@@ -0,0 +1,25 @@
+package japan.mori;
+
+import ca.tristan.easycommands.EasyCommands;
+import ca.tristan.easycommands.commands.defaults.HelpCmd;
+import net.dv8tion.jda.api.JDA;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class Main {
+
+    public static void main(String[] args){
+
+    }
+}
+/*throws IOException, URISyntaxException, InterruptedException {
+
+        EasyCommands ec = new EasyCommands();
+        jda = ec.registerListeners(
+
+        ).addExecutor(
+                new HelpCmd(ec)
+        ).addEnabledCacheFlags().addGatewayIntents().buildJDA();
+
+    }*/
